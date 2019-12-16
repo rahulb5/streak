672947c1aba8171130c7f809861e878147bdcd6d
rahulb5 <rhlbhagtani81@gmail.com>
2019-12-16 12:47:01 +0530

    Update deployed.py

--- streak/backtest/deployed.py
+++ streak/backtest/deployed.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-api_url = 'https://emt.edelweiss.in/edelmw-content/content/charts/v2/main/M1/NSE/EQUITY/11536_NSE'
+api_url = ''
 api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHAiOjAsImV4cCI6MTU2NDE2NjU2MCwiZmYiOiJNIiwiaXNzIjoiZW10IiwibmJmIjoxNTYxNTc0MjYwLCJhcHBpZCI6IjhiMDk2N2FlMDVkMDgzMmEyNTdlMzEyNzcxYWRmMjc2Iiwic3JjIjoiZW10bXciLCJpYXQiOjE1NjE1NzQ1NjAsImF2IjoiNC4xLjEiLCJiZCI6ImFuZHJvaWQtcGhvbmUifQ.PuKISoLOvi1cf0tY_zbivH2mc4yQE_EuosVBYEPpyN4'
 headers = {
     'accept': 'application/json',
